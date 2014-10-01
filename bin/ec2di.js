f20eb25d82377056020080b517d233624bcf5d84
Sam Morrison <sam@cnstechgroup.com>
2014-10-01 13:22:25 +0900

    simplify cli

--- bin/ec2di.js
+++ bin/ec2di.js
@@ -6,36 +6,16 @@ var chalk = require('chalk');
 var completion = require('completion');
 var ec2di = require('../lib/ec2di');
 
-
-/*program
-    .version(require('../package.json').version)
-    .option('-r, --refresh', 'Force refresh of cache by making API requests to EC2 (default: False - use cache files)')
-    .option('-i, --instance [instanceId]', 'Get all the variables about a specific instance')
-    .option('-v, --version', 'print version', false)
-    .parse(process.argv);*/
-
 var options = {};
 
-/*if (!process.argv || program.list) {
-    options.action = 'list';
-} else if (program.refresh) {
-    options.action = 'refresh';
-} else if (program.instance) {
-    options.action = 'instance';
-    options.instanceId = program.instance;
-} else if (program.help) {
-    program.help();
-}*/
-
 program
-    .version(pkg.version)
+    .version(require('../package.json').version)
 
 program
     .command("list")
-    .usage("List all EC2 / RDS instances.")
-    .option('-l, --list', 'List instances (default: True)')
-    .description("List all EC2 / RDS instances.")
-    .action(function() {
+    .usage("returns all EC2 and RDS instances for regions defined in config \n\t file (ec2.ini).")
+    .description("List all EC2 and RDS instances.")
+    .action(function(program) {
       
         var inventory = ec2di.emptyInventory();
         console.log(inventory);
@@ -43,26 +23,32 @@ program
     })
 
 program
-    .command("refresh-cache")
-    .usage("Refresh local inventory cache..")
-    .option('-r, --refresh-cache', 'Refresh local inventory cache.')
+    .command("refresh")
+    .usage("replaces the local inventory cache with the current EC2 / RDS \n\t inventory from AWS for regions defined in config file (ec2.ini).")
     .description("Refresh local inventory cache.")
-    .action(function() {
+    .action(function(program) {
       
         ec2di.doApiCallsUpdateCache();
 
     })
+
+program
+    .command("instance [instanceid]")
+    .usage("[instanceid] returns host data for the specified instance-id.")
+    .description("Get all the variables about a specific instance")
+    .action(function(instanceid, program) {
+      
+        ec2di.getInstance(instanceid);
+
+    })
+
+program.on("--help", function(){
+  console.log("  Use 'ec2di <command> --help' to get more information or visit https://github.com/scmorrison/ec2di to learn more.")
+  console.log('')
+})
 
 program.parse(process.argv)
 
 if(program.args.length < 1){
   program.help()
 }
-
-/*ec2di(options, function (err, inventory){
-
-  if(err) console.log(err);
-
-  console.log(inventory);
-
-});*/
