a00ca26d869978552eab7ad9cc0a5ab40d186199
Sam Morrison <sam@cnstechgroup.com>
2014-10-03 17:01:18 +0900

    modified:   bin/ec2di.js 	modified:   lib/ec2di.js 	modified:   package.json

--- bin/ec2di.js
+++ bin/ec2di.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
-
+/**
+ * cli interface to ec2di 
+ */
 var program = require('commander');
 var pkg = require("../package.json")
 var chalk = require('chalk');
 var completion = require('completion');
-var ec2di = require('../lib/ec2di');
+var ec2di = require("../lib/ec2di");
+//var ec2di = require('../lib/ec2di');
 
 var options = {};
 
+// Setup for command line argumens (see commander.js)
 program
     .version(require('../package.json').version)
 
@@ -33,6 +37,24 @@ program
     })
 
 program
+    .command("regions")
+    .usage("prints all know ec2 regions.")
+    .description("Print out ec2 regions.")
+    .action(function() {
+      
+         ec2di.getRegions(function(err, regions) {
+            if (err) console.log(err);
+            console.log(regions);
+        });
+
+        //log.success('Create ' + name + '@1.0.0 success!');
+
+        //var regions = ec2di.getRegions();  
+
+
+    })
+
+program
     .command("instance [instanceid]")
     .usage("[instanceid] returns host data for the specified instance-id.")
     .description("Get all the variables about a specific instance")
@@ -47,8 +69,11 @@ program.on("--help", function(){
   console.log('')
 })
 
+
+// Process passed arguments
 program.parse(process.argv)
 
+// Print help when no args
 if(program.args.length < 1){
   program.help()
 }
