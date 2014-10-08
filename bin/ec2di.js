7764237aa8147e685ab1597f818db5bd678c48cf
Sam Morrison <sam@cnstechgroup.com>
2014-10-08 11:21:25 +0900

    messing with module pattern / promises

--- bin/ec2di.js
+++ bin/ec2di.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-/**
- * cli interface to ec2di 
- */
-var program = require('commander');
-var pkg = require("../package.json")
-var chalk = require('chalk');
-var completion = require('completion');
-var ec2di = require("../lib/ec2di");
-//var ec2di = require('../lib/ec2di');
 
-var options = {};
+/**
+ * cli interface to ec2di
+ */
+var program = require('commander')
+var pkg = require("../package.json")
+var chalk = require('chalk')
+var completion = require('completion')
+var ec2di = require("../lib/ec2di")
+//var ec2di = require('../lib/ec2di')
+
+var options = {}
 
 // Setup for command line argumens (see commander.js)
 program
@@ -20,10 +21,9 @@ program
     .usage("returns all EC2 and RDS instances for regions defined in config \n\t file (ec2.ini).")
     .description("List all EC2 and RDS instances.")
     .action(function(program) {
-      
-        var inventory = ec2di.emptyInventory();
-        console.log(inventory);
 
+        var inventory = ec2di.inventory
+        console.log(inventory)
     })
 
 program
@@ -31,8 +31,8 @@ program
     .usage("replaces the local inventory cache with the current EC2 / RDS \n\t inventory from AWS for regions defined in config file (ec2.ini).")
     .description("Refresh local inventory cache.")
     .action(function(program) {
-      
-        ec2di.doApiCallsUpdateCache();
+
+        ec2di.doApiCallsUpdateCache()
 
     })
 
@@ -41,15 +41,15 @@ program
     .usage("prints all know ec2 regions.")
     .description("Print out ec2 regions.")
     .action(function() {
-      
-         ec2di.getRegions('ec2', function(err, regions) {
-            if (err) console.log(err);
-            console.log(regions);
-        });
 
-        //log.success('Create ' + name + '@1.0.0 success!');
+        ec2di.getRegions('ec2')
+             .then(function(regions) {
+                 console.log(regions)
+             })
 
-        //var regions = ec2di.getRegions();  
+        //log.success('Create ' + name + '@1.0.0 success!')
+
+        //var regions = ec2di.getRegions()  
 
 
     })
@@ -59,14 +59,14 @@ program
     .usage("[instanceid] returns host data for the specified instance-id.")
     .description("Get all the variables about a specific instance")
     .action(function(instanceid, program) {
-      
-        ec2di.getInstance(instanceid);
+
+        ec2di.getInstance(instanceid)
 
     })
 
-program.on("--help", function(){
-  console.log("  Use 'ec2di <command> --help' to get more information or visit https://github.com/scmorrison/ec2di to learn more.")
-  console.log('')
+program.on("--help", function() {
+    console.log("  Use 'ec2di <command> --help' to get more information or visit https://github.com/scmorrison/ec2di to learn more.")
+    console.log('')
 })
 
 
@@ -74,6 +74,6 @@ program.on("--help", function(){
 program.parse(process.argv)
 
 // Print help when no args
-if(program.args.length < 1){
-  program.help()
+if (program.args.length < 1) {
+    program.help()
 }
